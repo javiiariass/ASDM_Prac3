cdb6a60ef426e2f4fdbeee03ba276fcbcac533ac
javiiariass <javierariasfuentes@gmail.com>
2025-05-04 14:02:04 +0200

    Initialize project

---
+++ src/asdm_prac3/ASDM_Prac3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asdm_prac3;
+
+/**
+ *
+ * @author javi
+ */
+public class ASDM_Prac3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
